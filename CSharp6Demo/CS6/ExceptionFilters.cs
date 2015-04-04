bd0991cac3ba3b87e170529cee42056a11cad21b
Benedict Hazel <bwhazel@gmail.com>
2015-04-04 10:30:48 +0100

    Add C# 6 exception filters example.

---
+++ CSharp6Demo/CS6/ExceptionFilters.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="ExceptionFilters.cs" company="Benedict W. Hazel">
+//     Benedict W. Hazel, 2015
+// </copyright>
+// <author>Benedict W. Hazel</author>
+// <summary>
+//     ExceptionFilters: Class to demonstrate exception filters.
+// </summary>
+
+namespace BWHazel.Experiments.CSharp6Demo.CS6
+{
+	using System;
+	using static System.Console;
+
+	/// <summary>
+	/// Demonstrates exception filters.
+	/// </summary>
+	public static class ExceptionFilters
+	{
+		/// <summary>
+		/// Throws an exception which is filtered when caught.
+		/// </summary>
+		public static void Run()
+		{
+			try
+			{
+				throw new Exception("Error");
+			}
+			catch (Exception ex) when (ex.Message == "Error")
+			{
+				WriteLine("A terrible error has occurred!");
+			}
+			catch (Exception ex) when (ex.Message == "Fatal")
+			{
+				WriteLine("Something carastrophic has happened!");
+			}
+			catch (Exception ex)
+			{
+				WriteLine($"An unknown error has occurred: {ex.Message}");
+            }
+		}
+	}
+}
--- CSharp6Demo/Program.cs
+++ CSharp6Demo/Program.cs
@@ -23,8 +23,8 @@ namespace BWHazel.Experiments.CSharp6Demo
 		/// <param name="args">The command-line arguments.</param>
 		public static void Main(string[] args)
 		{
-			Program.RunCS5Examples();
-			// Program.RunCS6Examples();
+			// Program.RunCS5Examples();
+			Program.RunCS6Examples();
 
 			Console.ReadKey();
 		}
@@ -46,7 +46,8 @@ namespace BWHazel.Experiments.CSharp6Demo
 		{
 			// BWHazel.Experiments.CSharp6Demo.CS6.UsingStatic.Run();
 			// BWHazel.Experiments.CSharp6Demo.CS6.AutoProperties.Run();
-			BWHazel.Experiments.CSharp6Demo.CS6.DictionaryInitialisers.Run();
+			// BWHazel.Experiments.CSharp6Demo.CS6.DictionaryInitialisers.Run();
+			BWHazel.Experiments.CSharp6Demo.CS6.ExceptionFilters.Run();
 		}
 	}
 }
