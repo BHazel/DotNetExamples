30cdc9555d53c8962eb13eb0fba01acba6ff35d3
Benedict Hazel <bwhazel@gmail.com>
2015-04-04 10:36:44 +0100

    Add C# 5 equivalent of exception filters example.

---
+++ CSharp6Demo/CS5/ExceptionFilters.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="ExceptionFilters.cs" company="Benedict W. Hazel">
+//     Benedict W. Hazel, 2015
+// </copyright>
+// <author>Benedict W. Hazel</author>
+// <summary>
+//     ExceptionFilters: Class to demonstrate the C# 5 equivalent of exception filters.
+// </summary>
+
+namespace BWHazel.Experiments.CSharp6Demo.CS5
+{
+	using System;
+
+	/// <summary>
+	/// Demonstrates he C# 5 equivalent of exception filters.
+	/// </summary>
+	public static class ExceptionFilters
+	{
+		/// <summary>
+		/// Throws an exception which is filtered when caught.
+		/// </summary>
+		public static void Run()
+		{
+			try
+			{
+				throw new Exception("Error");
+			}
+			catch (Exception ex)
+			{
+				if (ex.Message == "Error")
+				{
+					Console.WriteLine("A terrible error has occurred!");
+				}
+				else if (ex.Message == "Fatal")
+				{
+					Console.WriteLine("Something carastrophic has happened!");
+				}
+				else
+				{
+					Console.WriteLine(string.Format("An unknown error has occurred: {0}", ex.Message));
+				}
+			}
+		}
+	}
+}
