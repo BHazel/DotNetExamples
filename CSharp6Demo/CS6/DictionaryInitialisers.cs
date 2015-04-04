9b7e6dcebbbff739584388c254a526f9df5bfc0f
Benedict Hazel <bwhazel@gmail.com>
2015-04-04 10:04:26 +0100

    Add C# 6 dictionary initialisers example.

---
+++ CSharp6Demo/CS6/DictionaryInitialisers.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="DictionaryInitialisers.cs" company="Benedict W. Hazel">
+//     Benedict W. Hazel, 2015
+// </copyright>
+// <author>Benedict W. Hazel</author>
+// <summary>
+//     DictionaryInitialisers: Class to demonstrate new dictionary initialisers.
+// </summary>
+
+namespace BWHazel.Experiments.CSharp6Demo.CS6
+{
+	using static System.Console;
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Demonstrates new dictionary initialisers.
+	/// </summary>
+	public static class DictionaryInitialisers
+	{
+		/// <summary>
+		/// Creates and populates a dictionary and prints its values to the console.
+		/// </summary>
+		public static void Run()
+		{
+			Dictionary<string, double> prices = new Dictionary<string, double>()
+			{
+				["Apples"] = 0.99,
+				["Beer"] = 2.99,
+				["Cola"] = 1.49
+			};
+
+			foreach (KeyValuePair<string, double> item in prices)
+			{
+				WriteLine($"{item.Key}: {item.Value}");
+            }
+		}
+	}
+}
--- CSharp6Demo/Program.cs
+++ CSharp6Demo/Program.cs
@@ -23,8 +23,8 @@ namespace BWHazel.Experiments.CSharp6Demo
 		/// <param name="args">The command-line arguments.</param>
 		public static void Main(string[] args)
 		{
-			Program.RunCS5Examples();
-			// Program.RunCS6Examples();
+			// Program.RunCS5Examples();
+			Program.RunCS6Examples();
 
 			Console.ReadKey();
 		}
@@ -44,7 +44,8 @@ namespace BWHazel.Experiments.CSharp6Demo
 		public static void RunCS6Examples()
 		{
 			// BWHazel.Experiments.CSharp6Demo.CS6.UsingStatic.Run();
-			BWHazel.Experiments.CSharp6Demo.CS6.AutoProperties.Run();
+			// BWHazel.Experiments.CSharp6Demo.CS6.AutoProperties.Run();
+			BWHazel.Experiments.CSharp6Demo.CS6.DictionaryInitialisers.Run();
 		}
 	}
 }
