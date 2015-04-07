a34d71fabfcfa7b8a540294121832265dc84c63f
Benedict Hazel <bwhazel@gmail.com>
2015-04-07 17:19:19 +0100

    Add configuration settings to control how the demos run.

--- CSharp6Demo/Program.cs
+++ CSharp6Demo/Program.cs
@@ -9,6 +9,10 @@
 namespace BWHazel.Experiments.CSharp6Demo
 {
 	using System;
+	using static System.Console;
+	using static System.Convert;
+	using System.Configuration;
+	using System.Reflection;
 
 	/// <summary>
 	/// Main program logic.
@@ -21,40 +25,55 @@ namespace BWHazel.Experiments.CSharp6Demo
 		/// <param name="args">The command-line arguments.</param>
 		public static void Main(string[] args)
 		{
-			Program.RunCS5Examples();
-			// Program.RunCS6Examples();
+			bool runDemosCSharp5 = GetVersionSettings("5");
+			bool runDemosCSharp6 = GetVersionSettings("6");
 
-			Console.ReadKey();
+			if (runDemosCSharp5 == true)
+			{
+				RunDemos("5");
+			}
+
+			if (runDemosCSharp6 == true)
+			{
+				RunDemos("6");
+			}
+
+			WriteLine("Press any key to exit...");
+			ReadKey();
 		}
 
 		/// <summary>
-		/// Runs the C# 5 examples.
+		/// Gets the app setting for the specifid version of C#.
 		/// </summary>
-		public static void RunCS5Examples()
+		/// <param name="cSharpVersion">The C# version as an integer.</param>
+		/// <returns><c>true</c> if this version of C# is set in app settings, otherwise <c>false</c>.</returns>
+		public static bool GetVersionSettings(string cSharpVersion)
 		{
-			// BWHazel.Experiments.CSharp6Demo.CS5.UsingStatic.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS5.AutoProperties.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS5.DictionaryInitialisers.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS5.ExceptionFilters.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS5.NameOf.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS5.StringInterpolation.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS5.NullConditional.Run();
-			BWHazel.Experiments.CSharp6Demo.CS5.ExpressionsAndStructs.Run();
+			return ToBoolean(ConfigurationManager.AppSettings[$"RunDemosCsharp{cSharpVersion}"]);
 		}
 
 		/// <summary>
-		/// Runs the C# 6 examples.
+		/// Runs the demos for the specified version of C#.
 		/// </summary>
-		public static void RunCS6Examples()
+		/// <param name="cSharpVersion">The C# version as an integer.</param>
+		public static void RunDemos(string cSharpVersion)
 		{
-			// BWHazel.Experiments.CSharp6Demo.CS6.UsingStatic.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS6.AutoProperties.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS6.DictionaryInitialisers.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS6.ExceptionFilters.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS6.NameOf.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS6.StringInterpolation.Run();
-			// BWHazel.Experiments.CSharp6Demo.CS6.NullConditional.Run();
-			BWHazel.Experiments.CSharp6Demo.CS6.ExpressionsAndStructs.Run();
+			WriteLine($"*** Running Demos for C# {cSharpVersion}... ***");
+            foreach (string key in ConfigurationManager.AppSettings.AllKeys)
+			{
+				if (key.StartsWith("RunDemosCSharp"))
+				{
+					continue;
+				}
+
+				if (ToBoolean(ConfigurationManager.AppSettings[key]) == true)
+				{
+					WriteLine($"*** Running Demo {key}... ***");
+					Type demoType = Assembly.GetExecutingAssembly().GetType($"BWHazel.Experiments.CSharp6Demo.CS{cSharpVersion}.{key}");
+					MethodInfo demoRunMethod = demoType.GetMethod("Run");
+					demoRunMethod.Invoke(demoType, null);
+				}
+            }
 		}
 	}
 }
