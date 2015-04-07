3b9990af626ec8d8bdd79e7147c83f623a0ca49d
Benedict Hazel <bwhazel@gmail.com>
2015-04-07 18:14:37 +0100

    Add C# 6 using await in catch blocks example.

---
+++ CSharp6Demo/CS6/CatchAwait.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="CatchAwait.cs" company="Benedict W. Hazel">
+//     Benedict W. Hazel, 2015
+// </copyright>
+// <author>Benedict W. Hazel</author>
+// <summary>
+//     CatchAwait: Class to demonstrate using "await" in "catch" blocks.
+// </summary>
+
+namespace BWHazel.Experiments.CSharp6Demo.CS6
+{
+	using System;
+	using static System.Console;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// Demonstrates using "await" in "catch" blocks.
+	/// </summary>
+	/// <remarks>
+	/// The delays in the "catch" and "finally" blocks will interfere will other demo output.
+	/// </remarks>
+	public static class CatchAwait
+	{
+		/// <summary>
+		/// Runs a method to throw and catch an exception.
+		/// </summary>
+		public static void Run()
+		{
+			CatchAwait.ThrowAndCatchException();
+		}
+
+		/// <summary>
+		/// Throws and handles an exception and asynchronously delays the task.
+		/// </summary>
+		public static async void ThrowAndCatchException()
+		{
+			try
+			{
+				throw new Exception("Error");
+			}
+			catch (Exception)
+			{
+				await Task.Delay(2000);
+				WriteLine("Caught Exception...");
+			}
+			finally
+			{
+				await Task.Delay(2000);
+				WriteLine("Finally...");
+			}
+		}
+	}
+}
